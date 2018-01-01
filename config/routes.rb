e15354e516e6faaf2c27e1c7cb7e89a37eb5ea21
marcos icaza <marcos.icaza@delta.com>
2018-01-01 14:08:11 -0700

    added a home page with information about the wedding

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'sites/index'
+
 	constraints subdomain: 'www' do
 	  get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
 	end
 	resources :rsvps
-
-	root 'rsvps#new'
+	root 'sites#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
